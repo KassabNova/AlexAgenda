d343c4a6f62b58227b39b7a41f158fe4aca98603
Carlos Kassab <carlos.kassab@nextiva.com>
2022-05-19 23:39:23 -0500

    Fixing connection string

--- src/main/java/meza/luis/agendav2/Metodos.java
+++ src/main/java/meza/luis/agendav2/Metodos.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package meza.luis.agendav2;
 
 import java.io.FileInputStream;
@@ -15,42 +16,40 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author ALUMNOS
  */
 public class Metodos {
-    static Logger login = 
+    static Logger login =
             Logger.getLogger(Metodos.class.getName());
-    
-    public Connection obtenerConexion()
-   {
+
+    public Connection obtenerConexion() {
         Connection conexion = null;
-        try{
-            Class.forName("net.ucanacces.jdbc.UcanaccessDriver");
-                String miBaseDatos = "jdbc:ucanaccess:///"
-                        + System.getProperty("user.dir")
-                        + "//Agendav2.accdb";
-                conexion = DriverManager.getConnection(miBaseDatos, "YO", "CHOCOMILK");
-        }catch(ClassNotFoundException fex){
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            //net.ucanaccess.jdbc.UcanaccessDriver test;
+
+            String miBaseDatos = "jdbc:ucanaccess:///"
+                    + System.getProperty("user.dir")
+                    + "//AgendaV2.accdb";
+            conexion = DriverManager.getConnection(miBaseDatos, "YO", "CHOCOMILK");
+        } catch (ClassNotFoundException fex) {
             login.log(Level.SEVERE, null, fex);
-            JOptionPane.showMessageDialog(null, "ERROR. DRIVER. \n" +fex);
-        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "ERROR. DRIVER. \n" + fex);
+        } catch (SQLException ex) {
             Logger.getLogger(Metodos.class.getName())
-                    .log(Level.SEVERE, null, "ERROR. CONEXION. \n"+ex);
+                    .log(Level.SEVERE, null, "ERROR. CONEXION. \n" + ex);
         }
-            return conexion;
+        return conexion;
     }
-    
-    public void Agregar(String nombre, String apellido, String domicilio, 
-            String email, String fechadenacimiento, int edad, String sexo, String telefono,
-            FileInputStream fis, int longitud){
+
+    public void Agregar(String nombre, String apellido, String domicilio,
+                        String email, String fechadenacimiento, int edad, String sexo, String telefono,
+                        FileInputStream fis, int longitud) {
+
         Connection conexion = obtenerConexion();
-        try{
-            String query = "insert into Personas"
-                    + "(Nombre, Apellido, Domicilio,"
-                    + "Email, FechadeNacimiento, Edad)"
-                    + "Sexo, Telefono, Foto"
-                    + "(?,?,?,?,?,?,?,?,?)";
+
+        try {
+            String query = "insert into Personas(Nombre, Apellido, Domicilio, Email, FechadeNacimiento, Edad, Sexo, Telefono, Foto) values (?,?,?,?,?,?,?,?,?)";
             PreparedStatement instruccion = conexion.prepareStatement(query);
             instruccion.setString(1, nombre);
             instruccion.setString(2, apellido);
@@ -63,28 +62,28 @@ public class Metodos {
             instruccion.setBinaryStream(9, fis, longitud);
             instruccion.executeUpdate();
             instruccion.close();
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(Metodos.class.getName())
-                  .log(Level.SEVERE, null, ex);
-        }finally{
-            try{
+                    .log(Level.SEVERE, null, ex);
+        } finally {
+            try {
                 conexion.close();
-            }catch(SQLException ex){
+            } catch (SQLException ex) {
                 login.log(Level.SEVERE, null, ex);
             }
         }
-    }  
-    
-    public String[] Buscar(String nombre){
+    }
+
+    public String[] Buscar(String nombre) {
         String[] resultados = new String[9];
         Connection conexion = obtenerConexion();
-        try{
+        try {
             String query = "select * from Personas where Nombre = ?";
             PreparedStatement instruccion = conexion.prepareStatement(query);
             instruccion.setString(1, nombre);
-            try{
+            try {
                 ResultSet rs = instruccion.executeQuery();
-                while(rs.next()){
+                while (rs.next()) {
                     resultados[0] = rs.getString("id");
                     resultados[1] = rs.getString("Nombre");
                     resultados[2] = rs.getString("Apellido");
@@ -96,52 +95,52 @@ public class Metodos {
                     resultados[8] = rs.getString("Telefono");
                 }
                 rs.close();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, "Exception: " + ex);
             }
             instruccion.close();
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(Metodos.class.getName())
-                  .log(Level.SEVERE, null, ex);
-        }finally{
-            try{
+                    .log(Level.SEVERE, null, ex);
+        } finally {
+            try {
                 conexion.close();
-            }catch(SQLException ex){
+            } catch (SQLException ex) {
                 login.log(Level.SEVERE, null, ex);
             }
         }
-        return resultados;                                                              
+        return resultados;
     }
-    
-        public byte[] obtenerFoto(int id){
+
+    public byte[] obtenerFoto(int id) {
         byte[] resultados = new byte[127];
         Connection conexion = obtenerConexion();
-        try{
+        try {
             String query = "select Foto from Personas where Id = ?";
             PreparedStatement instruccion = conexion.prepareStatement(query);
             instruccion.setInt(1, id);
-            try{
+            try {
                 ResultSet rs = instruccion.executeQuery();
-                while(rs.next()){
+                while (rs.next()) {
                     resultados = rs.getBytes("Foto");
                 }
                 rs.close();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null, "Exception: " + ex);
             }
             instruccion.close();
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(Metodos.class.getName())
-                  .log(Level.SEVERE, null, ex);
-        }finally{
-            try{
+                    .log(Level.SEVERE, null, ex);
+        } finally {
+            try {
                 conexion.close();
-            }catch(SQLException ex){
+            } catch (SQLException ex) {
                 login.log(Level.SEVERE, null, ex);
             }
         }
-        return resultados;                                                              
+        return resultados;
     }
-    
+
 }
 
